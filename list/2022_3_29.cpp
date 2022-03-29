8d57769d56fd2b9ed2617f70dc34c133bf122dfe
CCLCK <84182037+CCLCK@users.noreply.github.com>
2022-03-29 22:42:56 +0800

    链表测试（main函数）

---
+++ list/2022_3_29.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"list.h"
+
+//int main()
+//{
+//	int a = int();
+//	string b = string();
+//	double c = double();
+//	cout << a << endl;
+//	cout << b << endl;
+//	cout << c << endl;
+//	return 0;
+//}
+//匿名对象的用法
+
+//int main()
+//{
+//	//test1();
+//	//test2();
+//	/*test3();*/
+//	test4();
+//	return 0;
+//}
+
+
+
+
+
+
+
+
